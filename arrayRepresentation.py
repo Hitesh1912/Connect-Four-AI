43e26200e730d271ccf23ccd74e0fb3da53a2c74
Pradeepa Gopinath <pradeepagopinath19@gmail.com>
2017-11-20 22:35:39 -0500

    Update arrayRepresentation.py

--- arrayRepresentation.py
+++ arrayRepresentation.py
@@ -1,12 +1,37 @@
+import random
+
+def randomAgent(validRowValue):
+
+    randomcol= random.randint(0,6)
+    if validRowValue[randomcol]<5:
+        return randomcol
+    else:
+        randomcol(validRowValue)
+
+def agentMove(validRowValue):
+    move = random.randint(0, 6)
+    if validRowValue[move] < 5:
+        return move
+    else:
+        agentMove(validRowValue)
+
 arrayRep = [[0 for x in range(7)] for y in range(6)]
 flag=0
 arrayRep[0][3]=1
 print arrayRep
-rowValueWhenPlayedNext=[0 for x in range(7)]
-rowValueWhenPlayedNext[3]+=1
-print rowValueWhenPlayedNext
+validRowValue=[0 for x in range(7)]
+validRowValue[3]+=1
+print validRowValue
 
 while flag==0:
- randomAgentmove=randomAgent()
- agentColumnNumber=agentmove()
- break
+    agentColumnNumber=agentMove(validRowValue)
+    validRowValue[agentColumnNumber]+=1
+    arrayRep[validRowValue[agentColumnNumber]][agentColumnNumber]=1
+    print arrayRep
+    randomAgentMove=randomAgent(validRowValue)
+    validRowValue[agentColumnNumber] += 1
+    arrayRep[validRowValue[agentColumnNumber]][randomAgentMove]=2
+    print arrayRep
+    break
+
+
