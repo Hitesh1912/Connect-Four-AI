dcda4ffa0e158dc32d756ce9ce45131025447d9c
Hitesh <hitesh@Hiteshs-MacBook-Air.local>
2017-12-02 04:00:39 -0500

    removed extra spaces & unwanted print statement

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ def check_move(x, arrRep, validRowValue, playerNumber):
     return allowed, validRowValue
 
 
+# checking for game-over
 def game_over(arrRep, agent_number, opponent_number, current_column, validRowValue):
     isWinningMove = victory(arrRep, agent_number)
     if isWinningMove == 1:
@@ -23,90 +24,62 @@ def game_over(arrRep, agent_number, opponent_number, current_column, validRowVal
         if validRowValue[i] == 6:
             count += 1
     if count == 7:
-        return None,True
-    return None,False #No one has won
+        return None, True
+    return None, False # No one has won
 
 
+# checking for winning
 def victory(arrayRep, x):
     for i in range(len(arrayRep)):
-
         for j in range(len(arrayRep[i])):
-
             if arrayRep[i][j] == x:
-
                 if j == 3:
-
                     if (arrayRep[i][j + 1] == x and arrayRep[i][j + 2] == x and arrayRep[i][j + 3] == x) or (
                                         arrayRep[i][j - 1] == x and arrayRep[i][j - 2] == x and arrayRep[i][
                                     j - 3] == x):
-
-
                         return 1
 
                 elif j < 3:
-
-                    if (arrayRep[i][j + 1] == x and arrayRep[i][j + 2] == x and arrayRep[i][j + 3] == x):
-
-
+                    if arrayRep[i][j + 1] == x and arrayRep[i][j + 2] == x and arrayRep[i][j + 3] == x:
                         return 1
 
-                elif (arrayRep[i][j - 1] == x and arrayRep[i][j - 2] == x and arrayRep[i][j - 3] == x):
-
-
-
+                elif arrayRep[i][j - 1] == x and arrayRep[i][j - 2] == x and arrayRep[i][j - 3] == x:
                     return 1
 
     for i in range(len(arrayRep)):
-
         for j in range(len(arrayRep[i])):
-
             if arrayRep[i][j] == x:
-
                 if i <= 2:
-
-                    if (arrayRep[i + 1][j] == x and arrayRep[i + 2][j] == x and arrayRep[i + 3][j] == x):
-
+                    if arrayRep[i + 1][j] == x and arrayRep[i + 2][j] == x and arrayRep[i + 3][j] == x:
                         return 1
 
-                elif (arrayRep[i - 1][j] == x and arrayRep[i - 2][j] == x and arrayRep[i - 3][j] == x):
+                elif arrayRep[i - 1][j] == x and arrayRep[i - 2][j] == x and arrayRep[i - 3][j] == x:
                     return 1
 
     for i in range(len(arrayRep)):
-
         for j in range(len(arrayRep[i])):
-
             if arrayRep[i][j] == x:
-
                 if i <= 2 and j <= 3:
-                    if (arrayRep[i + 1][j + 1] == x and arrayRep[i + 2][j + 2] == x and arrayRep[i + 3][j + 3] == x):
-
+                    if arrayRep[i + 1][j + 1] == x and arrayRep[i + 2][j + 2] == x and arrayRep[i + 3][j + 3] == x:
                         return 1
 
                 if i <= 2 and j >= 3:
-
-                    if (arrayRep[i + 1][j - 1] == x and arrayRep[i + 2][j - 2] == x and arrayRep[i + 3][j - 3] == x):
-
-
+                    if arrayRep[i + 1][j - 1] == x and arrayRep[i + 2][j - 2] == x and arrayRep[i + 3][j - 3] == x:
                         return 1
 
                 if i >= 3 and j >= 3:
-
-                    if (arrayRep[i - 1][j - 1] == x and arrayRep[i - 2][j - 2] == x and arrayRep[i - 3][j - 3] == x):
-
-
+                    if arrayRep[i - 1][j - 1] == x and arrayRep[i - 2][j - 2] == x and arrayRep[i - 3][j - 3] == x:
                         return 1
 
                 if i >= 3 and j <= 3:
-                    if (arrayRep[i - 1][j + 1] == x and arrayRep[i - 2][j + 2] == x and arrayRep[i - 3][j + 3] == x):
-
-
+                    if arrayRep[i - 1][j + 1] == x and arrayRep[i - 2][j + 2] == x and arrayRep[i - 3][j + 3] == x:
                         return 1
 
     return 0
 
+# checking for tie
 def tie(arrayRep):
     for i in range(len(arrayRep)):
-
         for j in range(len(arrayRep[i])):
             if arrayRep[i][j]==0:
                 return False
