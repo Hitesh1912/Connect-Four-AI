c97185974c69ed91f55d8f0a3008f246c118f20c
HITESH VERMA <hitzver@gmail.com>
2017-12-12 22:06:22 -0500

    updated comments

--- connect4.py
+++ connect4.py
@@ -3,11 +3,11 @@ import game
 import agent
 import time
 
-#Alternate moves between agent,opponent and their representation on the turtle screen is handled in this method
+# Alternate moves between agent,opponent and their representation on the turtle screen is handled in this method
 def gameMoves():
-    #used to represent the game state internally. 1 represents agent's piece, 2 for opponent's piece and 0 for empty space
+    # used to represent the game state internally. 1 represents agent's piece, 2 for opponent's piece and 0 for empty space
     arrayRep = [[0 for x in range(7)] for y in range(6)]
-    #Has the valid row value of the next move for all the columns.
+    # Has the valid row value of the next move for all the columns.
     validRowValue = [0 for x in range(7)]
     #The agent plays in the centre column by default.Hence the values are updated in the arrays.
     arrayRep[0][3]=1
@@ -20,13 +20,13 @@ def gameMoves():
         ConnectFourGrid.printing_on_screen(rowNumber, colInputFromUser, 2)
         arrayRep[rowNumber][colInputFromUser] = 2
         validRowValue[colInputFromUser] += 1
-        #Victory condition is checked after the agent plays to see if it has resulted in victory.
+        # Victory condition is checked after the agent plays to see if it has resulted in victory.
         if game.victory(arrayRep,2)==1:
             print"Red won"
-            #sleep is added to freeze the turtle screen when the game ends.
+            # sleep is added to freeze the turtle screen when the game ends.
             time.sleep(5)
             break
-        #Tie happens when the whole grid is filled and there are no connect 4's for agent or opponent.
+        # Tie happens when the whole grid is filled and there are no connect 4's for agent or opponent.
         if game.tie(arrayRep):
             print "Match tied"
             time.sleep(5)
@@ -45,9 +45,9 @@ def gameMoves():
             time.sleep(5)
             break
 
-#Execution begins from here.
-#How to start the game:
-## Please enter the valid column number from 0-6 in the command prompt and press enter key to play the move
+# Execution begins from here.
+# How to start the game:
+# Please enter the valid column number from 0-6 in the command prompt and press enter key to play the move
 
 if __name__ == "__main__":
     gameMoves()
